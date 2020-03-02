239a07f42df4f72f566be72da05bd3439c44c6ae
jnqureshi <javeriaqureshi18@gmail.com>
2020-03-02 10:32:29 +0500

    Learning Vue - Part 2

--- js/vue.js
+++ js/vue.js
@@ -6,26 +6,33 @@ var app = new Vue({
 });
 
 //change the text
-app.hello = "New Text";
+// app.hello = "New Text";
 
-var app2 = new Vue({
-    el: '#app2',
-    data: {
-      seen: true
-    }
-  })
+// var app2 = new Vue({
+//     el: '#app2',
+//     data: {
+//       seen: true
+//     }
+//   })
   
-  app2.seen = true;
+//   app2.seen = true;
 
-  var app3 = new Vue({
-    el: '#app3',
-    data: {
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' },
-      ]
-    }
-  });
+//   var app3 = new Vue({
+//     el: '#app3',
+//     data: {
+//       todos: [
+//         { text: 'Learn JavaScript' },
+//         { text: 'Learn Vue' },
+//         { text: 'Build something awesome' },
+//       ]
+//     }
+//   });
   
-  app3.todos.push({ text: 'Learning Vue' });
+//   app3.todos.push({ text: 'Learning Vue' });
+
+//   var app5 = new Vue({
+//     el: '#app5',
+//     data: {
+//       message: 'Hello Vue!'
+//     }
+//   })
