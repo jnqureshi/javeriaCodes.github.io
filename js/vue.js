0e48c0bee72333bb34f7aa69ef066b3da8a98df9
jnqureshi <javeriaqureshi18@gmail.com>
2020-03-02 07:31:58 +0500

    Learning Vue

---
+++ js/vue.js
@@ -0,0 +1,31 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        hello: 'Hello World!'
+    }
+});
+
+//change the text
+app.hello = "New Text";
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+      seen: true
+    }
+  })
+  
+  app2.seen = true;
+
+  var app3 = new Vue({
+    el: '#app3',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },
+      ]
+    }
+  });
+  
+  app3.todos.push({ text: 'Learning Vue' });
